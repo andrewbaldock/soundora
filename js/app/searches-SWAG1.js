b52b4ef28bd32fa5ce3620645ef889831d33f9a0
Andrew Baldock <andrewbaldock@yahoo.com>
2013-08-15 21:06:57 -0700

    successfully rendering item to dom in bb

---
+++ js/app/searches-SWAG1.js
@@ -0,0 +1,135 @@
+/* github.com/andrewbaldock/soundora */
+
+define(["jquery", "json2", "backbone", "app/df_auth"], function($,Backbone,df_auth) {
+  aB.fn.Searches = function() {
+  	require(['backbone','app/df_auth'], function (Backbone, df_auth) {
+
+			Backbone.sync = function(method, model, success, error){
+				console.log('backbone sync');
+			}
+		
+			aB.Search = Backbone.Model.extend({
+				idAttribute: "id",
+				urlRoot: "https://dsp-song.cloud.dreamfactory.com/rest/db/userSearches/",
+				defaults: {
+					query: 'interscope',
+					userid: aB.userid
+				}
+			});
+
+			var List = Backbone.Collection.extend({
+				model: aB.Search,
+				url: "https://dsp-song.cloud.dreamfactory.com/rest/db/userSearches/",
+				parse : function(resp) {
+          return resp;
+        },
+				initialize: function(){		
+					this.fetch();
+				}
+			});
+			
+			var searchView = Backbone.View.extend({
+				//tagName: 'li', // name of (orphan) root tag in this.el
+				
+				events: {
+					'click a': 'doSearch',
+					'click .delete': 'deleteSearch'
+				},
+				
+				initialize: function(){
+					console.log("backbone initializing view");
+					_.bindAll(this, 'render', 'add', 'delete'); // every function that uses 'this' as the current object should be in here
+					this.model.bind('add', this.render);
+					this.model.bind('delete', this.unrender);
+		
+				},
+				render: function(){
+					$(this.el).append('<div class="asearch-wrap"><em class="icon-remove-circle delete"></em><a class="asearch" href="#">' + +this.model.get('query')+ + '</a></div>');	
+					return this; // for chainable calls, like .render().el
+					console.log('backbone rendered a search');
+				},
+				unrender: function(){
+					$(this.el).remove();
+					console.log('backbone unrendered a search');
+				}
+				
+			});
+
+			/* ListView class: Our main app view. */
+			aB.ListView = Backbone.View.extend({
+				el: $('#savedsearches'), // attaches `this.el` to an existing element.
+				initialize: function(){
+					// fixes loss of context for 'this' within methods
+					_.bindAll(this, 'render', 'addSearch', 'doSearch', 'deleteSearch'); 
+					this.collection = new List();
+					
+					// bind Backbone's Add event to my appendSearch function
+					this.collection.bind('add', this.appendSearch); 
+					
+					this.collection.fetch();
+					
+					this.render(); // not all views are self-rendering. This one is.
+				},
+
+				render: function(){
+					// run upon view init
+					var self = this; // so it can be accessed from the callback below
+					$(this.el).html('<h3>Your Searches</h3>');
+					
+					// in case collection is not empty
+					_(this.collection.models).each(function(search){ 
+						self.appendSearch(search);
+					}, this);
+					
+					console.log('backbone view loaded into dom');
+				},
+				
+				addSearch: function(){
+					var search = new aB.Search();
+					//get the search from the search field
+					var userinput = $('input#query').val().toLowerCase(); 
+					
+					//isDupe routine from stackoverflow.com/questions/6416958/
+					var isDupe = this.any(function(_search) { 
+							return _search.get('query') === search.get('query');
+					});
+					if (isDupe) {
+						console.log('dupe search detected, not creating');
+						return false;
+					};
+					search.set({
+						query: userinput 
+					});
+					// add search to collection; event 'add' fires appendSearch per init. to update the view
+					this.collection.add(search); 
+					console.log('search added');
+				},
+				
+				doSearch: function(search){
+					console.log('do search');
+					$('input#query').val( search.get('query') );
+					$('#thequery button').click();
+					
+				},
+				
+				deleteSearch: function(search){
+					console.log( 'delete search' + search.get('query') );
+					this.collection.delete(search);
+				},
+				
+				appendSearch: function(search){
+					var searchView = new searchView({
+						model: search
+					});
+					//relegate actual item redering to searchView
+					$(this.el).append(searchView.render().el);
+				}
+				
+			}); //end ListView class
+
+			// listView instance: Instantiate main app view. 
+			aB.listView = new aB.ListView();
+			
+    	})	
+  };
+});
--- js/app/startup.js
+++ js/app/startup.js
@@ -25,6 +25,7 @@ requirejs.config({
     "paths": {
       "app": "../app",
       "jquery" : 			"//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min",
+      //"jquery" : 			"//cdnjs.cloudflare.com/ajax/libs/zepto/1.0rc1/zepto.min",
       "json2" : 			"//cdnjs.cloudflare.com/ajax/libs/json2/20121008/json2.min",
       "underscore" :  "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.1/underscore-min",
       "backbone" : 		"//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min",
