c29ecc695f7206d884ae21cca83744e238f87eb5
Andrew Baldock <andrewbaldock@yahoo.com>
2013-08-24 20:56:11 -0700

    collection loading into view

--- js/app/searches.js
+++ js/app/searches.js
@@ -34,7 +34,7 @@ define(["jquery", "json2", "backbone", "app/df_auth"], function($,Backbone,df_au
 			//////////////////////////
 			var SearchCollection = Backbone.Collection.extend({
 				model: aB.Search,
-				url: aB.baseurl + "/db/searches?filter=userid%3D'" + aB.userid + "'&fields=id,query" ,
+				url: aB.baseurl + "/db/searches?filter=userid%3D'" + aB.userid + "'&fields=id,query",
 				parse: function(resp) {
 						console.log('pre-parse: "resp.record":');
 						console.log(resp.record);
@@ -48,23 +48,17 @@ define(["jquery", "json2", "backbone", "app/df_auth"], function($,Backbone,df_au
 			// 
 			//////////////////////////
 			var SearchView = Backbone.View.extend({
-				
 				el: $('#savedsearches'),
-				
 				initialize: function(){
         	this.el = $(this.el);
-        	this.render();
-    		},
-    		
+        },
 				events: {
 					'click .asearch': 'doSearch',
 					'click .delete': 'deleteSearch',
 					// Debug
         	'click #print-collection': 'printCollection'
 				},
-				
 				template: $('#searches-template').html(),
-
 				render: function(id,query) {
 						var templ = _.template(this.template);
 						var id = 
@@ -85,47 +79,42 @@ define(["jquery", "json2", "backbone", "app/df_auth"], function($,Backbone,df_au
 							.fadeOut(300,function() {
 									$(this).remove();
 							});
-    		},
+				},
     		printCollection: function(){
         	this.collection.each(function(item) {
             console.log(item.get('query'));
         	});
     		}
-
 			});
 
 			/*
-			START get remote data and begin. (use this OR section below)
+			START get remote data and begin
 			----------------------------------------------------------------*/
-
-			  if(typeof aB.searchCollection == 'undefined') {
-					aB.searchCollection = new SearchCollection();
-				} else {
-					//searchcollection exixsts.  refresh its url in case user is logged in
-					if(aB.userid != 'none') {
-						aB.searchCollection.url = aB.baseurl + "/db/searches?filter=userid%3D'" + aB.userid + "'&fields=id,query";
-					}
-				
+			if(typeof aB.searchCollection == 'undefined') {
+				aB.searchCollection = new SearchCollection();
+			} else {
+				//searchcollection exixsts.  refresh its url in case user is logged in
+				if(aB.userid != 'none') {
+					aB.searchCollection.url = aB.baseurl + "/db/searches?filter=userid%3D'" + aB.userid + "'&fields=id,query";
 				}
-			  
-
-				aB.searchCollection.fetch({
-						success: function() {
-								console.log('backbone got ' + aB.searchCollection.length + ' search records');
-								if(typeof aB.searchView == 'undefined') {
-									aB.searchView = new SearchView({collection: aB.searchCollection});
-								} else {
-									//push new results into existing collectionview
-									aB.searchView.render();
-								}
-						},
-						error: function() {
-								console.log('backbone collection activated: oh noes fetch fail');
-						}
-				}); 
-
-
-			
+			}
+			aB.searchCollection.fetch({
+					success: function() {
+							console.log('backbone got ' + aB.searchCollection.length + ' search records');
+							if(typeof aB.searchView == 'undefined') {
+								aB.searchView = new SearchView({collection: aB.searchCollection});
+								aB.searchCollection.each(function(item){
+									aB.searchView.render(item.get('id'),item.get('query'));
+								});
+							} else {
+								//push new results into existing collectionview
+								aB.searchView.render();
+							}
+					},
+					error: function() {
+							console.log('backbone collection activated: oh noes fetch fail');
+					}
+			}); 	
 			
     }) // end require	
   };  // end aB.fn.Searches
